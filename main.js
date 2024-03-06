87664f47b09d2cdd025aba7b87f32b9828f8b4f3
Alejo-26 <luisalejo2699@gmail.com>
2024-03-06 13:09:08 +0100

    Integration of Mobile Menu

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
 
 
 function toggleDesktopMenu(){
@@ -9,4 +12,9 @@ function toggleDesktopMenu(){
     // the toggle function will remove or put the class INACTIVE DEPENDING ON ITS CURRENT STATE
     desktopMenu.classList.toggle('inactive');
 }
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive')
+}
+
 console.log('JS working')
